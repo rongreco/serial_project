7b73379e384b845c9b1a99912b166ab58e2af0e1
rongreco <rishabhraj281@gmail.com>
2014-12-09 23:46:11 +0530

    1st phase
    1

---
+++ Serial_project/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BaudComboBox = New System.Windows.Forms.ComboBox()
+        Me.PortComboBox = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Readbutton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'BaudComboBox
+        '
+        Me.BaudComboBox.AccessibleName = "Baud"
+        Me.BaudComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.BaudComboBox.FormattingEnabled = True
+        Me.BaudComboBox.Items.AddRange(New Object() {"9600", "115200"})
+        Me.BaudComboBox.Location = New System.Drawing.Point(411, 125)
+        Me.BaudComboBox.Name = "BaudComboBox"
+        Me.BaudComboBox.Size = New System.Drawing.Size(121, 23)
+        Me.BaudComboBox.TabIndex = 0
+        '
+        'PortComboBox
+        '
+        Me.PortComboBox.AccessibleName = "Port"
+        Me.PortComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.PortComboBox.FormattingEnabled = True
+        Me.PortComboBox.Location = New System.Drawing.Point(411, 42)
+        Me.PortComboBox.Name = "PortComboBox"
+        Me.PortComboBox.Size = New System.Drawing.Size(121, 23)
+        Me.PortComboBox.TabIndex = 1
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.RichTextBox1.Location = New System.Drawing.Point(44, 49)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(285, 77)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(408, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Choose Port"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Output"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(408, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Choose Baud Rate"
+        '
+        'Readbutton
+        '
+        Me.Readbutton.Location = New System.Drawing.Point(569, 42)
+        Me.Readbutton.Name = "Readbutton"
+        Me.Readbutton.Size = New System.Drawing.Size(123, 37)
+        Me.Readbutton.TabIndex = 8
+        Me.Readbutton.Text = "Start Reading"
+        Me.Readbutton.UseVisualStyleBackColor = True
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(569, 155)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(123, 37)
+        Me.CloseButton.TabIndex = 9
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 261)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.Readbutton)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PortComboBox)
+        Me.Controls.Add(Me.BaudComboBox)
+        Me.Name = "Form1"
+        Me.Text = "IDE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BaudComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents PortComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Readbutton As System.Windows.Forms.Button
+    Friend WithEvents CloseButton As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Serial_project/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO
+Imports System.ComponentModel
+
+Public Class Form1
+    Dim myPort As Array
+    Delegate Function recieveData() As String
+    Delegate Function same(ByVal x As Integer, ByVal y As Integer) As Boolean
+    Function equalto(ByVal x As Integer, ByVal y As Integer) As Boolean
+        If x = y Then
+            Return True
+        Else : Return False
+        End If
+    End Function
+
+    Function ReceiveSerialData() As String
+        ' Receive strings from a serial port. 
+        Dim returnStr As String = ""
+
+        Dim com1 As IO.Ports.SerialPort = Nothing
+        Try
+            com1 = My.Computer.Ports.OpenSerialPort("COM3")
+            com1.ReadTimeout = 10000
+            Do
+                Dim Incoming As String = com1.ReadLine()
+                If Incoming Is Nothing Then
+                    Exit Do
+                Else
+                    returnStr &= Incoming & vbCrLf
+                End If
+            Loop
+        Catch ex As TimeoutException
+            returnStr = "Error: Serial Port read timed out."
+        Finally
+            If com1 IsNot Nothing Then com1.Close()
+        End Try
+
+        Return returnStr
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        PortComboBox.Items.AddRange(myPort)
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Readbutton.Click
+        SerialPort1.PortName = PortComboBox.Text                    'ianuragit
+        SerialPort1.BaudRate = BaudComboBox.Text
+        SerialPort1.Open()
+        Readbutton.Enabled = True
+        CloseButton.Enabled = True
+        Dim rsd As recieveData = AddressOf Me.ReceiveSerialData
+        Dim res As String = rsd.Invoke()
+        RichTextBox1.Text = res
+
+
+    End Sub
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Readbutton.Enabled = False
+        CloseButton.Enabled = False
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+End Class
